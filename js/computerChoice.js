307a017572054c22e06ab9cd7e3c49792bf717e6
shegridjsx <shegridjs.elearning@gmail.com>
2025-05-16 13:28:18 +0800

    Coded a script for ComputerChoice function for rock paper scissors game

---
+++ js/computerChoice.js
@@ -0,0 +1,15 @@
+let computerChoice;
+function getComputerChoice(max) {
+  computerChoice =  Math.floor(Math.random() * max);
+
+  if(computerChoice === 0){
+   return computerChoice = "Rock";
+  }
+  else if ( computerChoice === 1) {
+    return computerChoice = "Paper";
+  } else {
+    return computerChoice = "Scissor";
+  }
+}
+
+console.log(getComputerChoice(3));
