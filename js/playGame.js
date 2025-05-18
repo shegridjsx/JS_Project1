62272dee285b9ad5b728c9c71b5895edeb8b6afe
shegridjsx <shegridjs.elearning@gmail.com>
2025-05-18 18:29:11 +0800

    Developed a best of 5 series of the game Rock, Paper & Scissors. I also added scoring points logic on the script which determins the loser and the winner of the series.

---
+++ js/playGame.js
@@ -0,0 +1,33 @@
+
+function playGame(){
+
+    
+    let counter = 0;
+
+
+
+    for(counter; counter<5; counter++){
+
+        let humanChoice = prompt("Let\'s play Rock, Paper, & Scissor! \n\nThis is a best of 5 series. \n\nWhoever scores 3 first between you or the computer will win! \n\nPlease enter 'rock', 'paper', or 'scissor', your choice!\n\n Good luck!");
+        humanChoice = humanChoice.toLowerCase();
+
+
+        const humanSelection = getHumanChoice(humanChoice);
+        const computerSelection = getComputerChoice(3);
+
+        playround(humanSelection, computerSelection);
+    }
+    if(humanScore > computerScore){
+        console.log("\n\n\nYou win the series!\n");
+        console.log("\nScores:\n");
+        console.log("Human: " + humanScore);
+        console.log("Computer: " + computerScore);
+    } else {
+        console.log("\n\n\nYou lose the series!\n");
+        console.log("\nScores:\n");
+        console.log("Human: " + humanScore);
+        console.log("Computer: " + computerScore);
+    }
+}
+
+playGame();
